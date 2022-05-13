c085eea252711c51ad07a61a5b79d1de882897c9
Aatmodhee Goswami <atomind2005@gmail.com>
2022-05-13 13:11:59 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 var settings = {
     // common elements such as name, handles, etc
-    name: "Finn Lancaster",
+    name: "Aatmodhee Goswami",
     profile: "assets/images/me.png",
-    description: "Student, developer, ethical hacker, and pianist with a passion for the latest tech, AI, and the open-source community.",
-    email: "flancast90@gmail.com",
-    github: "flancast90",
+    description: "Aspiring Human",
+    email: "atomind2005@gmail.com",
+    github: "C0smicCrush",
 
     navLinks: [
         // all links must have a "text" attribute and a "link"
@@ -27,7 +27,7 @@ var settings = {
         },
         {
             text: "Resume",
-            link: "https://drive.google.com/file/d/1CywkswtJvVZVIcAXLmzmDbC-2n4_1Jr_/view",
+            link: "https://docs.google.com/document/d/1MF7uLVT1ATXBGMjeYe0C2K8AQPeTfNFY/edit?usp=sharing&ouid=111635364603016531402&rtpof=true&sd=true",
             external: true
         }
     ],
@@ -35,29 +35,29 @@ var settings = {
     // Experience timeline
     experience: [
         {
-            date: "August 2019 - June 2021",
-            title: "Santa Fe Catholic High School",
-            description: "Studied on accelerated dual-enrollment track."
+            date: "August 2020-",
+            title: "Austin Community College",
+            description: "Doing an Associate's degree in Computer Science along with a High School Diploma through Dual-Credit Programs"
         },
         {
-            date: "2020 - Present",
-            title: "Publix Supermarkets",
-            description: "Cashier and Customer Service clerk focused on promoting customer interactions and sales for the store"
+            date: "2021 - ",
+            title: "Tutor at Study Table",
+            description: "A tutor for Study Table LLC specializing in providing accelerated calculus curriculums for high schoolers and competitive academics preparation"
         },
         {
-            date: "2020 - Present",
-            title: "ARC Animal Rescue",
-            description: "Built ARC Animal Rescue website, including the creation and integration of an entirely new and dynamic Content Management System."
+            date: "2021 - ",
+            title: "ARC Animal Rescue web dev",
+            description: "Helped maintain the website for ARC Animal Rescue"
         },
-        {
-            date: "2021 - Present",
-            title: "Polk State College",
-            description: "Continued high school on accelerated learning course as a student within Polk State College in order to receive Associate of Arts degree upon high school graduation."
-        },
+//        {
+//           date: "2021 - ",
+//            title: "Polk State College",
+//            description: "Continued high school on accelerated learning course as a student within Polk State College in order to receive Associate of Arts degree upon high school graduation."
+//        },
         {
             date: "2022 - Present",
-            title: "Finned Ad Services",
-            description: "Built a software-as-a-service company promoting an Artificial Intelligence and analytics platform within the digital signage industry."
+            title: "CCO at Finned Ad Services",
+            description: "Focused on creating more enriching and better customer-side experiences for Finned"
         }
     ],
 
@@ -65,20 +65,20 @@ var settings = {
 
     contact: {
         linkedin: {
-            handle: "@finn-lancaster",
-            link: "https://www.linkedin.com/in/finn-lancaster-a21671219/"
+            handle: "@aatmodhee-goswami",
+            link: "https://www.linkedin.com/in/aatmodhee-goswami-4a305620a/"
         },
         github: {
-            handle: "@flancast90",
-            link: "https://github.com/flancast90"
+            handle: "@C0smicCrush",
+            link: "https://github.com/C0smicCrush"
         },
         discord: {
-            handle: "@flancast90",
+            handle: "@Studying#2893",
             link: "https://discord.com/channels/@me"
         },
         gmail: {
-            handle: "flancast90@gmail.com",
-            link: "mailto:flancast90@gmail.com"
+            handle: "atomind2005@gmail.com",
+            link: "mailto:atomind2005@gmail.com"
         }
     }
 }
